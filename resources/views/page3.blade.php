ed84df0e5ce2fc540ed6b42f22f2e39c8b09ce6a
mtrik <trikimabrouka09@gmail.com>
2023-11-13 11:51:23 +0100

    first commit

--- resources/views/page3.blade.php
+++ resources/views/page3.blade.php
@@ -1,15 +1,1 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Page d'accueil</title>
-</head>
-<body>
-<h1>Je suis dans la troisième page</h1>
-
-<p><a href="{{ url('/page1') }}">Page 1</a></p>
-<p><a href="{{ url('/page2') }}">Page 2</a></p>
-<p><a href="{{ url('/page3') }}">Page 3</a></p>
-<p><a href="{{ url('/') }}">Page principale</a></p>
-</body>
-</html>
-
+<?php
--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,70 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UtilisateurController;
+use App\Http\Controllers\SejourController;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-
-Route::get('/', function () {
-    return view('index1');
-});
-
-Route::get('/page1', function () {
-    return view('page1');
-});
-
-Route::get('/page2', function () {
-    return view('page2');
-});
-
-
-Route::get('/page3', function () {
-    return view('page3');
-});
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
 
 Route::get('/', function () {
-    return view('page3');
+    return view('home');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+/* * *********************************************************************** */
+/* * ******  Authentification ********************************************** */
+/* * *********************************************************************** */
+
+
+Route::get('/getLogin', function () {
+    return view('authentification/formLogin');
+});
+
+Route::get('/getLogout', [UtilisateurController::class, 'signOut']);
+
+Route::post('/login', [UtilisateurController::class, 'signIn']);
+
+/* * *********************************************************************** */
+/* * ******  Sejour ********************************************** */
+/* * *********************************************************************** */
+
+
+Route::get('/getListeSejour', [SejourController::class, 'listeSejours']);
+
+/*
+ * Ajout Séjour
+ */
+
+//get ajout
+Route::get('/ajoutSejour', [SejourController::class, 'ajoutSejour']);
+
+// post ajout
+Route::post('/ajoutSejour', [
+    'as' => 'postajoutSejour',
+    'uses' => 'App\Http\Controllers\SejourController@postajoutSejour'
+]);
+
+Route::get('/modifierSejour/{id}', [SejourController::class, 'modification']);
+
+//post modif
+Route::post('/postmodifierSejour/{id}',
+    [
+        'as' => 'postmodifierSejour',
+        'uses' => 'App\Http\Controllers\SejourController@postmodifierSejour'
+    ]);
+
+// suppression
+Route::get('/supprimerSejour/{id}', [SejourController::class, 'suppression']);
 
